999337a46588c61c35dde829b0fc950bfb60a017
Adam Goth <adamgoth@gmail.com>
2016-07-13 18:33:45 -0600

    Adds map, table view, location authorization

---
+++ dev-bootcamps/LocationVC.swift
@@ -0,0 +1,55 @@
+//
+//  LocationVC.swift
+//  dev-bootcamps
+//
+//  Created by Adam Goth on 7/13/16.
+//  Copyright © 2016 Adam Goth. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class LocationVC: UIViewController, UITableViewDelegate, UITableViewDataSource, MKMapViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var map: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        map.delegate = self
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        locationAuthStatus()
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
+    }
+    
+    func locationAuthStatus() {
+        if CLLocationManager.authorizationStatus() == .AuthorizedWhenInUse {
+            map.showsUserLocation = true
+        } else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+
+}
+
